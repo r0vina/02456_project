9b2e60a14cc15ca8e8773f5bc16e38f32179cdc6
Gustav Hansen <gustav5hansen5@gmail.com>
2023-01-04 10:01:22 +0100

    Change of parameters to run with

--- submit_train_test_run.sh
+++ submit_train_test_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #BSUB -J dlproject
-#BSUB -o dlproject_%J.out
-#BSUB -e dlproject_%J.err
+#BSUB -o /zhome/f9/2/183623/02456_project/outfiles/dlproject_%J.out
+#BSUB -e /zhome/f9/2/183623/02456_project/errorfiles/dlproject_%J.err
 #BSUB -q gpuv100
 #BSUB -gpu "num=1"
 #BSUB -n 1
@@ -19,10 +19,10 @@ module load cuda/11.7
 
 # activate the virtual environment
 source ../dl_env/bin/activate
-export LEARNING_RATE=0.001
-export BATCH_SIZE=4
+export LEARNING_RATE=0.005
+export BATCH_SIZE=16
 export EPOCHS=50
-export SCALE_SIZE=0.05
+export SCALE_SIZE=0.5
 #export CUDA_VISIBILE_DEVICES=0
 
-python3 02456_project/train_script_run.py
+python3 train_script_run.py
